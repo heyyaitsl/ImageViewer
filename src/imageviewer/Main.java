a2c57d9cc4d3e519b9ddee7dadcf7413adf76ad6
Laura <Laura@Laura-PC>
2017-01-10 19:02:51 +0000

    1

---
+++ src/imageviewer/Main.java
@@ -0,0 +1,8 @@
+package imageviewer;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ src/imageviewer/ui/ImageDisplay.java
@@ -0,0 +1,7 @@
+package imageviewer.ui;
+
+import imageviewer.model.Image;
+
+public interface ImageDisplay {
+    void display(Image image);
+}
